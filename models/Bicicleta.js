d8be4c8ecf9fd12127b9dde51281b3a57a42df8e
YerayGM <yeraygarciamartinez5@gmail.com>
2025-01-19 13:25:40 +0000

    sad

---
+++ models/Bicicleta.js
@@ -0,0 +1,19 @@
+let Bicicleta = function (id, color, modelo, ubicacion) {
+    this.id = id;
+    this.color = color;
+    this.modelo = modelo;
+    this.ubicacion = ubicacion;
+}
+
+Bicicleta.allBicis = [];
+
+Bicicleta.add = function (bici) {
+    this.allBicis.push(bici);
+}
+
+let a = new Bicicleta(1, "Rojo", "Trek", [28.503789, -13.853296]);
+let b = new Bicicleta(2, "Azul", "Orbea", [28.501367, -13.853476]);
+Bicicleta.add(a);
+Bicicleta.add(b);
+
+module.exports = Bicicleta;
---
+++ routes/api/bicicletas.js
@@ -0,0 +1,10 @@
+let express = require('express');
+let router = express.Router();
+let BicicletaControllerAPI = require("../../controllers/api/BicicletaControllerAPI");
+
+router.get("/", BicicletaControllerAPI.bicicleta_list);
+router.post("/create", BicicletaControllerAPI.bicicleta_create);
+router.delete("/delete", BicicletaControllerAPI.bicicleta_delete);
+router.put('/:id/update', BicicletaControllerAPI.bicicleta_update);
+
+module.exports = router;
